54302f18b2a6d6497f2bde4b82127166ad451922
Zakaria-Tafesh <zakaria.tafesh@gmail.com>
2023-09-01 01:25:12 +0300

    home to zones v2

--- flaskapp3-docker/flaskapp/website/views.py
+++ flaskapp3-docker/flaskapp/website/views.py
@@ -7,24 +7,24 @@ from . import db
 views = Blueprint('views', __name__)
 
 
+# @views.route('/', methods=['GET', 'POST'])
+# @login_required
+# def home():
+#     if request.method == 'POST':
+#         note = request.form.get('note')
+#         if len(note) < 1:
+#             flash('Note is too short', category='error')
+#         else:
+#             new_note = Note(data=note, user_id=current_user.id)
+#             db.session.add(new_note)
+#             db.session.commit()
+#             flash('Note Added!', category='success')
+#
+#     return render_template("home.html", user=current_user)
+
+
+
 @views.route('/', methods=['GET', 'POST'])
-@login_required
-def home():
-    if request.method == 'POST':
-        note = request.form.get('note')
-        if len(note) < 1:
-            flash('Note is too short', category='error')
-        else:
-            new_note = Note(data=note, user_id=current_user.id)
-            db.session.add(new_note)
-            db.session.commit()
-            flash('Note Added!', category='success')
-
-    return render_template("home.html", user=current_user)
-
-
-
-@views.route('/zones', methods=['GET', 'POST'])
 @login_required
 def zones_page():
     if request.method == 'POST':
