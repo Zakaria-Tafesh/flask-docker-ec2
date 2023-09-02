257d04047e0484f14dead058f70a4e48e0d58f7e
Zakaria-Tafesh <zakaria.tafesh@gmail.com>
2023-09-03 02:11:58 +0300

    Update Zone2 v3

--- flaskapp3-docker/flaskapp/website/static/zones.js
+++ flaskapp3-docker/flaskapp/website/static/zones.js
@@ -64,10 +64,10 @@ function updateZone1(zoneId) {
 }
 
 function updateZone2() {
-    let zone_id = document.getElementById("zone_id").innerText;
-    let client_name = document.getElementById("client_name").innerText;
-    let url = document.getElementById("url").innerText;
-    let payload = document.getElementById("payload").innerText;
+    let zone_id = document.getElementById("zone_id").innerHTML;
+    let client_name = document.getElementById("client_name").innerHTML;
+    let url = document.getElementById("url").innerHTML;
+    let payload = document.getElementById("payload").innerHTML;
     alert('Zone : ' + zone_id + client_name + url +  payload);
 
     // document.getElementById("client_name").innerHTML = zoneId ;
