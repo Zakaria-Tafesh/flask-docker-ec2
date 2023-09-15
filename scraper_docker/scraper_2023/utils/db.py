c0a850ae58c5851c01dd79280015a09801b11128
Zakaria-Tafesh <zakaria.tafesh@gmail.com>
2023-09-16 01:47:50 +0300

    Fix  import PATH_SHARED_DOCKER v2

--- scraper_docker/scraper_2023/utils/db.py
+++ scraper_docker/scraper_2023/utils/db.py
@@ -1,6 +1,6 @@
 import os
 import sqlite3
-from ..input.config import PATH_SHARED_DOCKER
+from scraper_docker.scraper_2023.input.config import PATH_SHARED_DOCKER
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
