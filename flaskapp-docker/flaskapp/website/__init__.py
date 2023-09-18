ed9fe2212204c1cd8568a3b580e48e6b06bf61e7
Zakaria-Tafesh <zakaria.tafesh@gmail.com>
2023-09-19 00:09:00 +0300

    convert database from sqlite to mysql

--- flaskapp-docker/flaskapp/website/__init__.py
+++ flaskapp-docker/flaskapp/website/__init__.py
@@ -7,13 +7,22 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 shared_docker = os.path.join('/', 'opt', 'shared_docker')
 db = SQLAlchemy()
 DB_NAME = 'database.db'
+# Get environment variables
+db_user = os.environ.get('MYSQL_USER')
+db_password = os.environ.get('MYSQL_PASSWORD')
+db_name = os.environ.get('MYSQL_DATABASE')
+secret_key = os.environ.get('SECRET_KEY')
+db_host = 'mysql'  # This should match the service name in your Docker Compose file
+
 
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'sdfsdfsdfsdfsdfsfsdfsdfsdf'
+    app.config['SECRET_KEY'] = secret_key
     # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(shared_docker, 'database.db')
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(shared_docker, 'database.db')
+    # Construct the database URI
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{db_user}:{db_password}@{db_host}/{db_name}'
     db.init_app(app)
 
 
