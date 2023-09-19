7d39da8e1de2d98200c2e140e7f7aac96fca99b0
Zakaria-Tafesh <zakaria.tafesh@gmail.com>
2023-09-20 00:26:20 +0300

    flask-mysql v4

--- mysql/entrypoint.sh
+++ mysql/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # Replace environment variables in the SQL template and create the SQL file
 sed -e "s/\${MYSQL_DATABASE}/$MYSQL_DATABASE/g" \
     -e "s/\${MYSQL_USER}/$MYSQL_USER/g" \
