b3dd81eddddce4e7c65d15a7d5af8487cfbe4cea
Zakaria-Tafesh <zakaria.tafesh@gmail.com>
2023-09-20 00:16:03 +0300

    flask-mysql v2

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Replace environment variables in the SQL template and create the SQL file
+sed -e "s/\${MYSQL_DATABASE}/$MYSQL_DATABASE/g" \
+    -e "s/\${MYSQL_USER}/$MYSQL_USER/g" \
+    -e "s/\${MYSQL_PASSWORD}/$MYSQL_PASSWORD/g" /docker-entrypoint-initdb.d/init_template.sql > /docker-entrypoint-initdb.d/init.sql
+
+# Start MySQL with the generated SQL file
+/docker-entrypoint.sh mysqld
