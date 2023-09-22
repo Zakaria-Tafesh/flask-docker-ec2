69c121ab7737f01d910a00e7c5a546dc4275f7f7
Zakaria-Tafesh <zakaria.tafesh@gmail.com>
2023-09-23 01:59:49 +0300

    mysql container v12

--- mysql/entrypoint.sh
+++ mysql/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-echo "Starting entrypoint.sh ..."
+#
+#echo "Starting entrypoint.sh ..."
 
 # Function to check if MySQL is up and running
 #mysql_is_up() {
@@ -14,4 +14,12 @@ echo "Starting entrypoint.sh ..."
 #done
 
 # Once MySQL is up, run your query script
-/usr/local/bin/query.sh
+#/usr/local/bin/query.sh
+
+# Wait for a few seconds to give MySQL time to initialize
+sleep 10
+
+# Run your query script
+exec /usr/local/bin/query.sh
+
+
