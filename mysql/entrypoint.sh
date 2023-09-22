d5fe93c41775649733456446a5d7c1a3e3424ac2
Zakaria-Tafesh <zakaria.tafesh@gmail.com>
2023-09-23 01:51:54 +0300

    mysql container v10

--- mysql/entrypoint.sh
+++ mysql/entrypoint.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-# Function to check if MySQL is up and running
-mysql_is_up() {
-  mysqladmin ping -h localhost --silent
-}
+echo "Starting entrypoint.sh ..."
 
-# Wait for MySQL to become available
-while ! mysql_is_up; do
-  echo "Waiting for MySQL to become available..."
-  sleep 10
-done
+# Function to check if MySQL is up and running
+#mysql_is_up() {
+#  mysqladmin ping -h localhost --silent
+#}
+#
+## Wait for MySQL to become available
+#while ! mysql_is_up; do
+#  echo "Waiting for MySQL to become available..."
+#  sleep 10
+#done
 
 # Once MySQL is up, run your query script
 /usr/local/bin/query.sh
--- mysql/query.sh
+++ mysql/query.sh
@@ -7,20 +7,20 @@ mysql_is_up() {
 }
 
 # Wait for MySQL to become available
-while ! mysql_is_up; do
-  echo "Waiting for MySQL to become available..."
-  sleep 5
-done
-
-echo "MySQL is available now ..."
-
-MYSQL_USER=${MYSQL_USER}
-MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}
-MYSQL_DATABASE=${MYSQL_DATABASE}
-MYSQL_PASSWORD=${MYSQL_PASSWORD}
-
-
-QUERY="SELECT CONCAT('The user is: ', '$MYSQL_USER');"
-
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" -S /var/run/mysqld/mysqld.sock -e "$QUERY"
+#while ! mysql_is_up; do
+#  echo "Waiting for MySQL to become available..."
+#  sleep 5
+#done
+#
+#echo "MySQL is available now ..."
+#
+#MYSQL_USER=${MYSQL_USER}
+#MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}
+#MYSQL_DATABASE=${MYSQL_DATABASE}
+#MYSQL_PASSWORD=${MYSQL_PASSWORD}
+#
+#
+#QUERY="SELECT CONCAT('The user is: ', '$MYSQL_USER');"
+#
+#mysql -u root -p"$MYSQL_ROOT_PASSWORD" -S /var/run/mysqld/mysqld.sock -e "$QUERY"
 
