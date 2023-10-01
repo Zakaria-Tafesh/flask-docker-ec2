33b631c6fa683f05ad2147fbf7db7c8ba980855a
Zakaria-Tafesh <zakaria.tafesh@gmail.com>
2023-10-01 23:45:34 +0300

    User : password from 150 to 1000

--- flaskapp-docker/flaskapp/website/models.py
+++ flaskapp-docker/flaskapp/website/models.py
@@ -38,7 +38,7 @@ class Zone(db.Model):
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String(150))
+    password = db.Column(db.String(1000))
     first_name = db.Column(db.String(150))
     notes = db.relationship('Note')
     # zones = db.relationship('Zone')
