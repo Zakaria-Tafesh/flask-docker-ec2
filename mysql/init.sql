ee987c2dd50c205f674ed8ed7dacdfd2c51c9322
Zakaria-Tafesh <zakaria.tafesh@gmail.com>
2023-10-01 17:25:19 +0300

    add init.sql v2

--- mysql/init.sql
+++ mysql/init.sql
@@ -3,12 +3,8 @@ SELECT 'Debugging message 1';
 
 SELECT '${MYSQL_USER}', '${MYSQL_PASSWORD}', '${MYSQL_DATABASE}';
 
-CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-
--- Use the created database
-USE ${MYSQL_DATABASE};
-
--- Create the user and grant privileges
-CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
-
+-- Create a sample table
+CREATE TABLE IF NOT EXISTS my_table2 (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name2 VARCHAR(255) NOT NULL
+);
